17d765d9da1968ad627e299ced176c3cb5c6e125
Radislav Střílek <96394461+Astat7@users.noreply.github.com>
2024-01-30 18:08:32 +0100

    Update prog.js

--- prog.js
+++ prog.js
@@ -1,10 +1,52 @@
+// recipe description toggle extended menu
+
 function showInfo(obj) {
     let descr = obj.parentElement.getElementsByClassName("desc")[0];
     let styl = window.getComputedStyle(descr);
 
-    if (styl.visibility == "hidden") {
-        descr.style.visibility = "visible";
+    if (styl.display == "none") {
+        descr.style.display = "initial";
     }else{
-        descr.style.visibility = "hidden";
+        descr.style.display = "none";
     }
+}
+
+
+// filter
+
+const whiteBox = document.getElementById("whitelistBox");
+const forms = Array.from(document.getElementById("sidebar").getElementsByTagName("form"));
+const recipes = Array.from(document.getElementById("menu").getElementsByClassName("dish"));
+
+let boxes = [];
+forms.forEach(function(item){
+    boxes = boxes.concat(Array.from(item.getElementsByTagName("input")));
+});
+
+
+function toggleDisplay(name, val){
+    recipes.forEach(function(item){
+        console.log(item)
+        if(item.classList.contains(name)){
+            item.style.display = val;
+        }
+    });
+}
+
+function filter(){
+    boxes.forEach(function(item){
+        if(whiteBox.checked == false){
+            if(item.checked == true){
+                toggleDisplay(item.id.replace("Box", ""), "none");
+            }else{
+                toggleDisplay(item.id.replace("Box", ""), "block");
+            };
+        }else{
+            if(item.checked == false){
+                toggleDisplay(item.id.replace("Box", ""), "none");
+            }else{
+                toggleDisplay(item.id.replace("Box", ""), "block");
+            };
+        };
+    });
 }
