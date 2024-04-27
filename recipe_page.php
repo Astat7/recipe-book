26bae73aa6ce50f5adde01a7ef1a4a2df61b08db
Radislav Střílek <96394461+Astat7@users.noreply.github.com>
2024-04-27 15:19:21 +0200

    Update recipe_page.php

--- recipe_page.php
+++ recipe_page.php
@@ -1,11 +1,37 @@
 <?php
 require_once("SmartCookClient.php");
 
+$htmlText = file_get_contents("recipe_page_template.html");
+
+$prices = array(1=>"cheap", 2=>"medium", 3=>"expensive");
+$difficulties = array(1=>"easy", 2=>"medium", 3=>"hard");
+
 try {
-    (new SmartCookClient)
+    $data = (new SmartCookClient)
         ->setRequestData(["recipe_id" => $_GET["recipe_id"]])
         ->sendRequest("recipe")
-        ->printResponse();
+        ->getResponseData();
 } catch (Exception $e) {
     echo $e->getMessage();
 }
+
+$data["data"]["price"] = $prices[$data["data"]["price"]];
+$data["data"]["difficulty"] = $difficulties[$data["data"]["difficulty"]];
+$data["data"]["country"] = strtoupper($data["data"]["country"]);
+
+foreach ($data["data"] as $key => $value) {
+    if ($key == "ingredient") {
+        $arrVal = [];
+        foreach ($value as $ingredient) {
+            array_push($arrVal, $ingredient["name"]);
+        }
+        $arrVal = implode(", ", $arrVal);
+        $htmlText = str_replace('{'.$key.'}', $arrVal, $htmlText);
+    } elseif (is_array($value)) {
+        $arrVal = implode(", ", $value);
+        $htmlText = str_replace('{'.$key.'}', $arrVal, $htmlText);
+    } else {
+        $htmlText = str_replace('{'.$key.'}', $value, $htmlText);
+    }
+}
+echo $htmlText;
